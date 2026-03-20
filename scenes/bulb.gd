2b32318c9108a166ca2038f866dd0b1e09d5cf0e
Recheescript. <rechiejames.postanes@bisu.edu.ph>
2026-03-20 17:26:52 +0800

    Add and configure 3D light bulb scene with shadow disabling and static lighting

---
+++ scenes/bulb.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@onready var light = $OmniLight3D
+@onready var audio = $AudioStreamPlayer3D
+
+@export var base_energy: float = 0.8
+
+func _ready():
+	light.light_energy = base_energy
+	_disable_shadows(self)
+
+func _disable_shadows(node: Node):
+	if node is GeometryInstance3D:
+		# Godot 4 constant for disabling shadows on a mesh!
+		node.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	for child in node.get_children():
+		_disable_shadows(child)
