2724de868b9eff0877254623097babc1a48ce256
Recheescript. <rechiejames.postanes@bisu.edu.ph>
2026-04-03 21:44:43 +0800

    Updated flashlight shadow settings and scene fixes

--- scenes/tv.gd
+++ scenes/tv.gd
@@ -7,42 +7,42 @@ extends Node3D
 @export var is_on: bool = false
 
 func _ready():
-    add_to_group("Electronics")
-    # Setup the view port texture dynamically over the TV screen mesh
-    var mat = StandardMaterial3D.new()
-    mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-    mat.albedo_texture = $SubViewport.get_texture()
-    tv_screen.material_override = mat
-    
-    _update_state()
+	add_to_group("Electronics")
+	# Setup the view port texture dynamically over the TV screen mesh
+	var mat = StandardMaterial3D.new()
+	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat.albedo_texture = $SubViewport.get_texture()
+	tv_screen.material_override = mat
+	
+	_update_state()
 
 func turn_off():
-    is_on = false
-    _update_state()
+	is_on = false
+	_update_state()
 
 func toggle():
-    is_on = not is_on
-    _update_state()
+	is_on = not is_on
+	_update_state()
 
 func interact():
-    toggle()
+	toggle()
 
 func get_interaction_prompt() -> String:
-    if is_on:
-        return "Turn Off"
-    else:
-        return "Turn On"
+	if is_on:
+		return "Turn Off"
+	else:
+		return "Turn On"
 
 func _update_state():
-    if is_on:
-        video_player.play()
-        screen_light.visible = true
-        if tv_screen.material_override:
-            tv_screen.material_override.albedo_color = Color(1, 1, 1, 1)
-        $SubViewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
-    else:
-        video_player.stop()
-        screen_light.visible = false
-        if tv_screen.material_override:
-            tv_screen.material_override.albedo_color = Color(0, 0, 0, 1)
-        $SubViewport.render_target_update_mode = SubViewport.UPDATE_DISABLED
+	if is_on:
+		video_player.play()
+		screen_light.visible = true
+		if tv_screen.material_override:
+			tv_screen.material_override.albedo_color = Color(1, 1, 1, 1)
+		$SubViewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	else:
+		video_player.stop()
+		screen_light.visible = false
+		if tv_screen.material_override:
+			tv_screen.material_override.albedo_color = Color(0, 0, 0, 1)
+		$SubViewport.render_target_update_mode = SubViewport.UPDATE_DISABLED
